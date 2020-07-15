b5cfe6509373c24f13a63a6a77de09bd73ff950e
Joe S <joe@skrd.uk>
2020-07-15 10:35:22 +0100

    Define initial Laravel package

---
+++ src/ShopServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jskrd\Shop;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShopServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
