880efaa1924a85f90dfb81ab677b8a93e621c352
Joe S <joe@skrd.uk>
2020-07-16 13:59:00 +0100

    Try keep factory created Product names unique

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,7 +5,7 @@ use Jskrd\Shop\Product;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'name' => ucwords($faker->words(rand(1, 3), true)),
+        'name' => ucwords($faker->unique()->words(rand(1, 3), true)),
         'options1' => ucfirst($faker->word),
         'options2' => ucfirst($faker->word),
         'options3' => ucfirst($faker->word),
