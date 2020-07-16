b121b47d53fa3733b038189706171e44a1d98548
Joe S <joe@skrd.uk>
2020-07-16 13:58:53 +0100

    Try keep factory created Variant names unique

--- database/factories/VariantFactory.php
+++ database/factories/VariantFactory.php
@@ -6,7 +6,7 @@ use Jskrd\Shop\Variant;
 
 $factory->define(Variant::class, function (Faker $faker) {
     return [
-        'name' => ucwords($faker->words(rand(1, 3), true)),
+        'name' => ucwords($faker->unique()->words(rand(1, 3), true)),
         'price' => rand(100, 10000),
         'delivery_cost' => rand(100, 1000),
         'stock' => rand(0, 1) === 1 ? rand(1, 10) : null,
