016b09b824b95afea68e14548aecdcd6711bd158
Joe S <joe@skrd.uk>
2020-07-17 16:40:23 +0100

    Support configuring API path

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,22 @@
 <?php
 
-Route::middleware('api')
-    ->namespace('Jskrd\Shop\Http\Controllers')
-    ->prefix('shop-api')
-    ->group(function () {
-        Route::apiResource('addresses', 'AddressController')
-            ->only(['store']);
+use Illuminate\Support\Facades\Route;
 
-        Route::apiResource('baskets', 'BasketController')
-            ->only(['store', 'show']);
+Route::apiResource('addresses', 'AddressController')->only(['store']);
 
-        Route::apiResource('baskets.variants', 'BasketVariantController');
+Route::apiResource('baskets', 'BasketController')->only(['store', 'show']);
 
-        Route::apiResource('products', 'ProductController')
-            ->only(['index', 'show']);
+Route::apiResource('baskets.variants', 'BasketVariantController');
 
-        Route::apiResource('products.images', 'ProductImageController')
-            ->only(['index']);
+Route::apiResource('products', 'ProductController')->only(['index', 'show']);
 
-        Route::apiResource('products.variants', 'ProductVariantController')
-            ->only(['index']);
+Route::apiResource('products.images', 'ProductImageController')
+    ->only(['index']);
 
-        Route::apiResource('variants', 'VariantController')
-            ->only(['show']);
+Route::apiResource('products.variants', 'ProductVariantController')
+    ->only(['index']);
 
-        Route::apiResource('variants.images', 'VariantImageController')
-            ->only(['index']);
-    });
+Route::apiResource('variants', 'VariantController')->only(['show']);
+
+Route::apiResource('variants.images', 'VariantImageController')
+    ->only(['index']);
--- src/ShopServiceProvider.php
+++ src/ShopServiceProvider.php
@@ -2,19 +2,42 @@
 
 namespace Jskrd\Shop;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class ShopServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->configure();
+
         $this->loadFactoriesFrom(__DIR__ . '/../database/factories');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+
+        $this->publishes([
+            __DIR__ . '/../config/shop.php' => config_path('shop.php'),
+        ]);
+
+        $this->registerRoutes();
+    }
+
+    protected function configure(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/shop.php', 'shop');
     }
 
     public function register(): void
     {
         //
+    }
+
+    protected function registerRoutes(): void
+    {
+        Route::middleware('api')
+            ->namespace('Jskrd\Shop\Http\Controllers')
+            ->prefix(config('shop.path'))
+            ->group(function () {
+                $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+            });
     }
 }
