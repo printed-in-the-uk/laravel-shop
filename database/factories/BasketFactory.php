d20cb1ee2d3928919e872afdaafc84b0724d8880
Joe S <joe@skrd.uk>
2020-08-19 20:42:03 +0100

    Fake discount on factory created Baskets

--- database/factories/BasketFactory.php
+++ database/factories/BasketFactory.php
@@ -3,10 +3,13 @@
 use Faker\Generator as Faker;
 use Jskrd\Shop\Address;
 use Jskrd\Shop\Basket;
+use Jskrd\Shop\Discount;
 
 $factory->define(Basket::class, function (Faker $faker) {
     return [
+        'discount_amount' => rand(100, 1000),
         'delivery_cost' => rand(100, 1000),
+        'discount_id' => factory(Discount::class),
         'billing_address_id' => factory(Address::class),
         'delivery_address_id' => factory(Address::class),
     ];
